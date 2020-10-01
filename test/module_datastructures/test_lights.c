1e15ab45d5f33b32cf66f82e8fd320e14db0799a
Zachary D. Rowitsch <rowitsch@yahoo.com>
2020-09-30 22:54:10 -0400

    add some missing testing for lights

--- test/module_datastructures/test_lights.c
+++ test/module_datastructures/test_lights.c
@@ -15,6 +15,28 @@ void test_pointlight_has_position_and_intensity() {
     LIGHTS_delete_pointlight(pl);
 }
 
+void test_light_copy() {
+    TUPLES_Color* c = TUPLES_new_color(0.1, 0.2, 0.3);
+    TUPLES_Point* p = TUPLES_new_point(0.4, 0.5, 0.6);
+    LIGHTS_PointLight* pl = LIGHTS_new_pointlight(p, c);
+    LIGHTS_PointLight pl_copy;
+    LIGHTS_copy(&pl_copy, pl);
+    TEST_ASSERT_TRUE(TUPLES_is_equal(c, &pl_copy.intensity));
+    TEST_ASSERT_TRUE(TUPLES_is_equal(p, &pl_copy.position));
+    TUPLES_delete_all(c, p);
+    LIGHTS_delete_pointlight(pl);
+}
+
+void test_get_color() {
+    TUPLES_Color* c = TUPLES_new_color(0.1, 0.2, 0.3);
+    TUPLES_Point* p = TUPLES_new_point(0.4, 0.5, 0.6);
+    LIGHTS_PointLight* pl = LIGHTS_new_pointlight(p, c);
+    const TUPLES_Color* c_returned = LIGHTS_get_color(pl);
+    TEST_ASSERT_TRUE(TUPLES_is_equal(c, c_returned));
+    TUPLES_delete_all(c, p);
+    LIGHTS_delete_pointlight(pl);
+}
+
 int main(void)
 {
     UNITY_BEGIN();
