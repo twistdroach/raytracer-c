a30eb00271a2e5ee65e4337e3ddf1e4a4e145b78
Zachary D. Rowitsch <rowitsch@yahoo.com>
2020-10-23 23:27:22 -0400

    clang-format commit 3

---
+++ build-debug/module_utilities/CMakeFiles/module_utilities.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/zrowitsch/src/raytracer-c/module_utilities/exceptions.c" "/home/zrowitsch/src/raytracer-c/build-debug/module_utilities/CMakeFiles/module_utilities.dir/exceptions.c.o"
+  "/home/zrowitsch/src/raytracer-c/module_utilities/logger.c" "/home/zrowitsch/src/raytracer-c/build-debug/module_utilities/CMakeFiles/module_utilities.dir/logger.c.o"
+  "/home/zrowitsch/src/raytracer-c/module_utilities/utilities.c" "/home/zrowitsch/src/raytracer-c/build-debug/module_utilities/CMakeFiles/module_utilities.dir/utilities.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "UNITY_DOUBLE_PRECISION=0.0001f"
+  "UNITY_INCLUDE_DOUBLE"
+  "UNITY_INCLUDE_EXEC_TIME"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../module_utilities"
+  "../module_utilities/CException"
+  "../external/CException/lib"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/zrowitsch/src/raytracer-c/build-debug/external/CMakeFiles/CException.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build-debug/module_utilities/CMakeFiles/module_utilities.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/module_utilities.dir/exceptions.c.o"
+  "CMakeFiles/module_utilities.dir/logger.c.o"
+  "CMakeFiles/module_utilities.dir/utilities.c.o"
+  "libmodule_utilities.a"
+  "libmodule_utilities.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/module_utilities.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
